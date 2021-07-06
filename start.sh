96da8d9b506e248c5e972e4f22c490241cabe2ac
ccruz <cesar@logicsp.com.br>
2021-07-06 08:42:18 -0300

    initial

---
+++ start.sh
@@ -0,0 +1,14 @@
+#./utils/launch.sh --vnc 172.16.50.62:5900
+
+
+kill -9 `sudo lsof -t -i:6080`
+kill -9 `sudo lsof -t -i:8888`
+
+echo "Link exemplo http://localhost:8888/vnc_lite.html?host=localhost&port=6080&path=websockify/?token=target1"
+
+cd ./utils
+./launch.sh --listen 8888 &
+cd websockify
+./run --token-plugin TokenFile --token-source ./token/ 6080 &
+
+
