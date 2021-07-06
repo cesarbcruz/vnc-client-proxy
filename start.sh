10e9a2ae894220244bc8639b7ad050b35060de87
ccruz <cesar@logicsp.com.br>
2021-07-06 09:58:48 -0300

    create dockerfile

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #./utils/launch.sh --vnc 172.16.50.62:5900
-#kill -9 `lsof -t -i:6080`
-#kill -9 `lsof -t -i:8888`
+kill -9 `lsof -t -i:6080`
+kill -9 `lsof -t -i:8888`
 
 echo "Link exemplo http://localhost:8888/vnc_lite.html?host=localhost&port=6080&path=websockify/?token=target1"
 cd ./utils
