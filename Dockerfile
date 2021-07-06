1bccd6676c3a3fc5d1787f80f1ef2386324c904b
ccruz <cesar@logicsp.com.br>
2021-07-06 09:50:16 -0300

    create dockerfile

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:3.7-buster
+WORKDIR /usr/src/app
+COPY . .
+RUN chmod +x /usr/src/app/start.sh
+
+EXPOSE 8888
+EXPOSE 6080
+
+ENTRYPOINT /usr/src/app/start.sh
--- start.sh
+++ start.sh
@@ -1,14 +1,13 @@
 #./utils/launch.sh --vnc 172.16.50.62:5900
-
-
-kill -9 `sudo lsof -t -i:6080`
-kill -9 `sudo lsof -t -i:8888`
+#kill -9 `lsof -t -i:6080`
+#kill -9 `lsof -t -i:8888`
 
 echo "Link exemplo http://localhost:8888/vnc_lite.html?host=localhost&port=6080&path=websockify/?token=target1"
-
 cd ./utils
+ls
 ./launch.sh --listen 8888 &
+ls
 cd websockify
-./run --token-plugin TokenFile --token-source ./token/ 6080 &
+./run --token-plugin TokenFile --token-source ./token/ 6080 
 
 
